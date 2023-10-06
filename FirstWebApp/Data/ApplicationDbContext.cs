e73cda5d7b5a44da7c01b04a175ae122a72d6638
HELLIOS300 <syahputra.ardhana@gmail.com>
2023-10-06 19:39:16 +0700

    Add project files.

---
+++ FirstWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
